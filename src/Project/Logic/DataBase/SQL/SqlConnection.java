7b4630dcbb4bc2f82aaf624e2a2b8ab530b961d2
pooyaj21 <105147486+pooyaj21@users.noreply.github.com>
2023-08-10 18:49:00 +0330

    jar file made

--- src/Project/Logic/DataBase/SQL/SqlConnection.java
+++ src/Project/Logic/DataBase/SQL/SqlConnection.java
@@ -5,14 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SqlConnection {
-//    private final static String url = "jdbc:mysql://localhost:3306/FinalProject";
+//    private final static String url = "jdbc:mysql://85.190.22.134:3501/ngi";
 //    private final static String username = "root";
-//    private final static String password = "pooya1234";
+//    private final static String password = "Pooya@6742";
 //
 //    public static Connection getConnection() throws SQLException {
 //        return DriverManager.getConnection(url, username, password);
 //    }
 
+    static {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
     private static final String DATABASE_URL = "jdbc:sqlite:DB/FinalProject.db";
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DATABASE_URL);
